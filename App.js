5112202ec304c929ff9e36511ab2e382ca0d635c
Yasir Naeem <yasirnaeem@hwryk.com>
2024-01-17 04:12:46 -0800

    change

--- App.js
+++ App.js
@@ -6,8 +6,10 @@ import FlashMessage from 'react-native-flash-message';
 export default function App() {
   return (
     <>
+    <View style={{flex:1}}>
     <FlashMessage position='bottom' />
       <AppRouter />
+      </View>
     </>
   );
 }
