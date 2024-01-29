e244e0f370196a4dbd813781d3665ee743125ea7
Yasir Naeem <arslanali@hwryk.com>
2024-01-29 18:04:06 +0500

    google login configuration on IOS

--- Component/AuthFeild/GoogleAuth.js
+++ Component/AuthFeild/GoogleAuth.js
@@ -2,6 +2,7 @@ import {
     GoogleSignin,
     GoogleSigninButton,
     statusCodes,
+
 } from '@react-native-google-signin/google-signin';
 
 
@@ -13,7 +14,7 @@ export default function GoogleAuth() {
         // hostedDomain: '', // specifies a hosted domain restriction
         // forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
         // accountName: '', // [Android] specifies an account name on the device that should be used
-        // iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+        iosClientId: '40923534712-is15f3mnn148cmgvmrbjqdkuflsnij52.apps.googleusercontent.com', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
         // googleServicePlistPath: '', // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
         // openIdRealm: '', // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
         // profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
--- Screens/HomeCreateAcount/index.js
+++ Screens/HomeCreateAcount/index.js
@@ -15,6 +15,9 @@ GoogleSignin.configure({
     // scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
     webClientId: '40923534712-nsgs2aud99q4115v2m9ofocvlhb7hqb3.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
     offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+  
+    iosClientId: '40923534712-is15f3mnn148cmgvmrbjqdkuflsnij52.apps.googleusercontent.com', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+        
     // hostedDomain: '', // specifies a hosted domain restriction
     // forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
     // accountName: '', // [Android] specifies an account name on the device that should be used
@@ -130,7 +133,7 @@ function CreateAcountHOme({ navigation }) {
             // await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
 
             const userInfo = await GoogleSignin.signIn();
-            // console.log(userInfo)
+            console.log(userInfo)
             if (userInfo) {
                 navigation.navigate('HomeScreen')
             }
